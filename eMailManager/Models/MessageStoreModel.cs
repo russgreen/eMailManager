65cbf90a8bd2cb9d2aa4ec436ea507b579793a62
Russ Green <russ.green@live.co.uk>
2021-04-27 16:12:04 +0100

    added models

---
+++ eMailManager/Models/MessageStoreModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMailManager.Models
+{
+    public class MessageStoreModel
+    {
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Path { get; set; }
+
+        public string FolderID { get; set; }
+
+        public string StoreID { get; set; }
+
+        public bool IsOutlookPath 
+        {
+            get
+            {
+                //if the path = folder id then its not an outlook folder
+                if (Path == FolderID)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ eMailManager/Models/SettingsModel.cs
@@ -0,0 +1,51 @@
+﻿using eMailManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eMailManager.Models
+{
+    public class SettingsModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public ObservableCollection<MessageStoreModel> MessageStores = new ObservableCollection<MessageStoreModel>();
+
+        public string FilenameFilter { get; set; } = "<sent>_<from>_<subject>";
+
+        // OUTLOOK SAVEAS FILE TYPES
+        // 0	-	olTXT	-	Text format (.txt)
+        // 1	-	olRTF	-	Rich Text format (.rtf)
+        // 2	-	olTemplate	-	Microsoft Office Outlook template (.oft)
+        // 3	-	olMSG	-	Outlook message format (.msg)
+        // 4	-	olDoc	-	Microsoft Office Word format (.doc)
+        // 5	-	olHTML	-	HTML format (.html)
+        // 6	-	olVCard	-	VCard format (.vcf)
+        // 7	-	olVCal	-	VCal format (.vcs)
+        // 8	-	olICal	-	iCal format (.ics)
+        // 9	-	olMSGUnicode	-	Outlook Unicode message format (.msg)
+        // 10	-	olMHTML	-	MIME HTML format (.mht)
+        public int MessageSaveAsType { get; set; } = 3;        
+        public string MessageFileExt { get; set; } = ".msg";
+        public bool RememberLastLocation { get; set; } = false;
+        public string LastLocation { get; set; } = string.Empty;
+        public string ArchivedAndRetainedCategory { get; set; } = "eMail Manager Filed";
+        public bool LeaveCopy { get; set; } = false;
+        public bool AlwaysEmbedAttachments { get; set; } = false;
+        public bool AlwaysClearDeletedItems { get; set; } = false;
+        public bool MonitorSentItems { get; set; } = true;
+        public bool UseFileSystem { get; set; } = true;
+        public bool AutoArchiveIn { get; set; } = false;
+        public bool AutoArchiveOut { get; set; } = false;
+        public string AutoArchiveInPath { get; set; } = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public string AutoArchiveOutPath { get; set; } = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public int FormWidth { get; set; } = 705;
+        public int FormHeight { get; set; } = 520;
+
+    }
+}
